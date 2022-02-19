e74f2a9379c226556f76995779589aeaad946e50
alexP-bu <apozin@bu.edu>
2022-02-18 22:12:15 -0500

    wordle now works !

--- views/js/script.js
+++ views/js/script.js
@@ -4,6 +4,9 @@ import guesses from './guesses.js'
 $(document).ready(function () {
     $('.help').hide();
     $('.player_stats').hide();
+    $('.invalid_word').hide();
+    $('.enter_key').hide();
+    $('.error_text').hide();
 
     $('.x_icon').click(function (e) { 
         $('.help').hide();
@@ -32,15 +35,85 @@ $(document).ready(function () {
     //game code, client-size implementation first
     var row = 1;
     var col = 1;
-    var wordle = answers[5];
+    var wordle = answers[Math.floor((Math.random() * ((answers.length - 1) - 0 + 1)))];
+    var curString = "";
+    var curLetter = document.querySelector('.letter_grid div:nth-child(' + row + ') div:nth-child(' + col + ')');
     console.log(wordle);
     $('.key').click(function (event) {
-        if(col == 6){
-
-        }else{
-            var curLetter = document.querySelector('.letter_grid div:nth-child(' + row + ') div:nth-child(' + col + ')');
+        curLetter = document.querySelector('.letter_grid div:nth-child(' + row + ') div:nth-child(' + col + ')');
+        if(col <= 5){
             $(curLetter).text($(event.target).text());
+            curString += $(event.target).text();
             col++;
-        } 
+        }
+        if(curString.length == 5){
+            $('.key').hide();
+            $('.enter_key').show();
+        }else{
+            $('.key').show();
+            $('.enter_key').hide();
+        }
+    });
+
+    $('.backspace_key').click(function (e) { 
+        if(curString.length > 0){
+            curString = curString.substring(0, curString.length - 1);
+            curLetter = document.querySelector('.letter_grid div:nth-child(' + row + ') div:nth-child(' + (col - 1) + ')');
+            $(curLetter).text("");
+            col--;        
+            console.log(curString);
+            console.log(col);
+        }
+        if(curString.length == 5){
+            $('.key').hide();
+            $('.enter_key').show();
+        }else{
+            $('.key').show();
+            $('.enter_key').hide();
+        }
+    });
+
+    $('.enter_key').click(function (e) { 
+        //win condition
+        if(curString == wordle){
+            for(var i = 0; i < 5; i++){
+                var letter = document.querySelector('.letter_grid div:nth-child(' + row + ') div:nth-child(' + (i+1) + ')');
+                $(letter).css("background-color", "rgb(83,141,78)");
+            }
+            $('.error_text').text("YOU WIN!");
+            $('.error_text').fadeIn(1000).fadeOut(3000);
+            $('.enter_key').hide();
+            $('.backspace_key').hide();
+        }else{
+            //otherwise, we didnt win
+            if(row > 5){
+                $('.error_text').text("YOU LOSE!");
+                $('.error_text').css("color", "red");
+                $('.error_text').show();
+                $('.enter_key').hide();
+                $('.backspace_key').hide();
+                $('.key').hide();
+            }
+            if(guesses.has(curString)){
+                for(var i = 0; i < curString.length; i++){
+                    var letter = document.querySelector('.letter_grid div:nth-child(' + row + ') div:nth-child(' + (i+1) + ')');
+                    if(curString.charAt(i) == wordle.charAt(i)){
+                        $(letter).css("background-color", "rgb(83,141,78)");
+                    }else if(wordle.includes(curString.charAt(i))){
+                        $(letter).css("background-color", "rgb(181,159,59)"); 
+                    }else{
+                        $(letter).css("background-color", "rgb(58,58,60)");
+                    }
+                }
+                row++;
+                col = 1;
+                curString = "";
+                $('.key').show();
+                $('.enter_key').hide();
+            }else{
+                $('.error_text').text("Word is not in list");
+                $('.error_text').fadeIn(1000).fadeOut(2000);
+            }
+        }
     });
 });
